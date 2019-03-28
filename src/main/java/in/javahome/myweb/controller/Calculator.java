a55568a4abfdc0262ae5ee9c4865f37175d43ed9
Deybison <dytoro@gmail.com>
2019-03-28 10:19:53 -0500

    update

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,20 @@
+package in.javahome.myweb.controller;
+
+/*
+ *
+ */
+public class Calculator {
+    /*
+     * @param i
+     * @param j
+     * @return int
+     */
+    public int add(int i, int j) {
+        String s = null;
+        return i + j;
+    }
+
+    public int multiply(int i, int j) {
+        return i * j;
+    }
+}
